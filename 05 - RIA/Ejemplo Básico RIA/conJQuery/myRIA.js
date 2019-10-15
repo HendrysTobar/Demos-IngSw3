c1603af78827f4044d421d023a7fcef8e3a93a18
Hendrys Tobar <fabiantobar@gmail.com>
2019-10-15 09:28:13 -0500

    Corrección nombres carpetas RIA

--- 05 - RIA/Ejemplo Básico RIA/conJQuery/myRIA.js
+++ 05 - RIA/Ejemplo Básico RIA/conJQuery/myRIA.js
@@ -1,35 +1,10 @@
 //
 alert("Bienvenido a la página RIA");
 //
-function getdata()
-{
-    //
-    if (this.readyState == 4 && this.status == 200)
-    {
-        //
-        document.getElementById("canasta").innerHTML = this.responseText;
-    }    
-}
-
-//
-function EnviarSolicitudXML(edad)
-{
-    //
-     var xhttp = new XMLHttpRequest();
-     //
-     xhttp.onreadystatechange = getdata;
-     //
-     xhttp.open("GET", "calcularbebida.php?edad=" + edad);
-     //
-     xhttp.send();
-}
-
-//
 function CalcularSiPuedoBeberRIA()
 {
     //
     var edad = document.getElementById("txtEdad").value;
     //
-    EnviarSolicitudXML(edad);
-
+    $("#canasta").load("calcularbebida.php?edad=" + edad);
 }
